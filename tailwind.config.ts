cf3254cdb74b511392cd073006419788d47f5dcb
Blackman <DBlackman@int.co.chenango.ny.us>
2024-12-04 15:02:12 -0500

    end of day

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'beige100': "hsl(23, 6%, 57%",
       },
     },
   },
